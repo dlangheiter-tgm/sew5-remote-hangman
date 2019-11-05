07a2f33c77b3ef1f71b05bb59494f13a698b1c13
TheKingDave <david@langheiter.com>
2019-11-05 15:14:52 +0100

    Added arg check to server

--- server/bin/main.dart
+++ server/bin/main.dart
@@ -1,5 +1,9 @@
 import 'package:server/server.dart' as server;
 
 main(List<String> arguments) async {
+  if(arguments.length != 1) {
+    print("Usage: server <host>");
+    return;
+  }
   await server.Server(int.parse(arguments.first)).run();
 }
