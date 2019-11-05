e72ca8e5c88dd0f1bcbdc1deb52ac7ce7ee1f8a6
TheKingDave <david@langheiter.com>
2019-11-05 14:27:06 +0100

    Poc server impl

--- server/lib/server.dart
+++ server/lib/server.dart
@@ -1,3 +1,30 @@
-int calculate() {
-  return 6 * 7;
+
+
+import 'dart:io';
+
+class Server {
+  final int port;
+  int clientNum = 0;
+
+  Server(this.port);
+
+  run() async {
+    final srv = await ServerSocket.bind(InternetAddress.loopbackIPv4, port);
+    print("Serving at ${srv.address}:${srv.port}");
+    srv.listen(handleClient);
+  }
+
+  void handleClient(Socket client) async {
+    print("New client accepted #$clientNum");
+    await for(var req in client) {
+      String _in = String.fromCharCodes(req);
+      if(_in.trim().isEmpty) {
+        continue;
+      }
+      print("Req: $_in");
+      client.write("Hi\n");
+    }
+    print("Client disconnected");
+  }
+
 }
