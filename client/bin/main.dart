b5ea29407a844651f1ff882ceafdb2bb0b635538
TheKingDave <david@langheiter.com>
2019-11-05 15:32:42 +0100

    Client working

--- client/bin/main.dart
+++ client/bin/main.dart
@@ -1,9 +1,10 @@
 import 'package:client/client.dart' as client;
 
-main(List<String> arguments) {
+main(List<String> arguments) async {
   if(arguments.length != 2) {
     print("Usage: client <host> <port>");
     return;
   }
-  client.Client(arguments[0], int.parse(arguments[1]));
+  final c = client.Client(arguments[0], int.parse(arguments[1]));
+  await c.run();
 }
--- client/lib/client.dart
+++ client/lib/client.dart
@@ -1,9 +1,35 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:io';
+
+import 'dart:typed_data';
+
 class Client {
   final String host;
   final int port;
 
   Client(this.host, this.port);
 
+  void run() async {
+    Stream<String> input =
+        stdin.transform(utf8.decoder).transform(LineSplitter());
 
+    Socket server = await Socket.connect(host, port);
 
+    input.listen((line) {
+      server.writeln(line.trim());
+    });
+
+    server.listen(_handleServer);
+
+    print("end");
+  }
+
+  void _handleServer(Uint8List data) {
+    String _in = String.fromCharCodes(data);
+    if(_in.trim().isEmpty) {
+      return;
+    }
+    print(_in.trim());
+  }
 }
